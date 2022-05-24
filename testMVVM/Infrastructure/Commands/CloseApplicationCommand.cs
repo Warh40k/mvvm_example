03f05da751504430197bd6d0c3bc51f723cc275b
Никита <kommunist.bear@gmail.com>
2022-05-24 14:28:43 +0300

    Add command in file and ellipse

---
+++ testMVVM/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using testMVVM.Infrastructure.Commands.Base;
+
+namespace testMVVM.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
