b7bbf1a0979882c3132f071d4e15746710c0f565
Никита <kommunist.bear@gmail.com>
2022-05-24 10:35:55 +0300

    working binding

--- testMVVM/ViewModels/MainWindowViewModel.cs
+++ testMVVM/ViewModels/MainWindowViewModel.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using testMVVM.ViewModels.Base;
 
 namespace testMVVM.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна 
+
+        private string _Title = "Поиск аномалий";
+
+        /// <summary>  Заголовок окна </summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    // 1) Можно так
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnPropertyChanged();
+
+            //    //2) Можно и так
+            //    //Set(ref _Title, value);
+            //}
+            //3) Но самое жесткое
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
     }
 }
